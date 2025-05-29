a28bf41c88d844bed9f58ea9b67fc56409ccd792
Christian Curbelo <chris94curbelo@gmail.com>
2025-05-29 18:07:20 -0300

    subiendo inventory

--- src/Library/Items/Inventory.cs
+++ src/Library/Items/Inventory.cs
@@ -2,5 +2,46 @@
 
 public class Inventory
 {
-    
+    private List<IItem> items = new List<IItem>();
+
+    public void AddItem(IItem item)
+    {
+        this.items.Add(item);
+    }
+
+    public void RemoveItem(IItem item)
+    {
+        this.items.Remove(item);
+    }
+
+    public int AttackValue
+    {
+        get
+        {
+            int value = 0;
+            foreach (IItem item in this.items)
+            {
+                if (item is IAttackItem attackItem)
+                    value += attackItem.AttackValue;
+            }
+
+            return value;
+        }
+    }
+
+    public int DefenseValue
+    {
+        get
+        {
+            int value = 0;
+            foreach (IItem item in this.items)
+            {
+                if (item is IDefenseItem defenseItem)
+                    value += defenseItem.DefenseValue;
+            }
+
+            return value;
+        }
+    }
 }
+
